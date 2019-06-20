8689b30109afa59f36ef8cddcd929b2f826dbd76
banshang <maoxiangtian@gmail.com>
2019-06-20 16:41:41 +0800

    day 5

--- awesome-website/www/app.py
+++ awesome-website/www/app.py
@@ -1,8 +1,40 @@
 import logging; logging.basicConfig(level=logging.INFO)
-import asyncio
+import asyncio, os, json, time
 from aiohttp import web
+from datetime import datetime
+from jinja2 import Environment, FileSystemLoader
+
+## config配置会在后面添加
+from config import configs
 
 
+import orm
+from coroweb import add_routes, add_static
+
+## handlers是url处理模块，当handlers.py在API章节里完全编辑完再将下一行代码的双#去掉
+## from handlers import cookie2user, COOKIE_NAME
+
+## 初始化jinja2的函数
+def init_jinja2(app, **kw):
+    logging.info('init jinja2...')
+    options = dict(
+        autoscape = kw.get('autoscape', True),
+        block_start_string = kw.get('block_start_string', '{%'),
+        block_end_string = kw.get('block_end_string', '%}'),
+        variable_start_string = kw.get('variable_start_string', '{{'),
+        variable_end_string = kw.get('variable_end_string', '}})'),
+        auto_reload = kw.get('auto_reload', True)
+    )
+    path = kw.get('auto_reload', True)
+    if path is None:
+        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templetes')
+        logging.info('set jinja2 template path: %s' % path)
+        env = Environment(loader=FileSystemLoader(path), **options)
+        filters = kw.get('filters', None)
+        if filter is not None:
+            for name, f in filters.items():
+                env.filters[name] = f
+        app['__templating__'] = env
 #定义服务器响应请求的返回为“Awesome Website”
 async def index(request):
     return web.Response(body=b'<h1>Awesome Website</h1>', content_type='text/html')
--- awesome-website/www/coroweb.py
+++ awesome-website/www/coroweb.py
@@ -20,6 +20,7 @@ def get(path):
         return wrapper
     return decorator
 
+
 def post(path):
     def decorator(func):
         @functools.wraps(func)
@@ -30,6 +31,7 @@ def post(path):
         return wrapper
     return decorator
 
+
 def get_required_kw_args(fn):
     args = []
     params = inspect.signature(fn).parameters
@@ -38,6 +40,7 @@ def get_required_kw_args(fn):
         and param.default == inspect.Parameter.empty:
             args.append(name)
     return tuple(args)
+
 
 def get_named_kw_args(fn):
     args = []
@@ -100,9 +103,9 @@ class RequestHandler(object):
                                                   be object.')
                     kw = params
                 elif ct.startswith('application/x-www-form-urlencoded')\
-                     or ct.startswith('multipart/form-data'):
-                        params = await request.post()
-                        kw = dict(**params)
+                  or ct.startswith('multipart/form-data'):
+                    params = await request.post()
+                    kw = dict(**params)
                 else:
                     return web.HTTPBadRequest(text='Unsupported Content-Type:\
                                             %s' % request.content_type)
@@ -124,11 +127,59 @@ class RequestHandler(object):
                     kw = copy
                 # check named arg:
                 for k, v in request.match_info.items():
-                    if k in kwL
-                    logging.warning('Duplicate arg name in named arg and\
+                    if k in kw:
+                        logging.warning('Duplicate arg name in named arg and\
                                     kw args: %s' % k)
                     kw[k] = v
             if self._has_request_arg:
+                kw['request'] = Request
+            # check required kw:
+            if self._required_kw_args:
+                for name in self._required_kw_args:
+                    if not name in kw:
+                        return web.HTTPBadRequest(text='Missing argument: %s' % name)
+            logging.info('call with args: %s' % st(kw))
+            try:
+                r = await self._func(**kw)
+                return r
+            except APIError as e:
+                return dict(error=e.error, data=e.data, message=e.message)
 
 
+## 定义add_static函数，来注册static文件夹下的文件
+def add_static(app):
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+    app.router.add_static('/static/', path)
+
+
+## 定义add_route函数，来注册一个URL处理函数
+def add_route(app, fn):
+    method = getattr(fn, '__method__', None)
+    path = getattr(fn, '__route__', None)
+    if path is None or method is None:
+        raise ValueError('@get or @post not defined in %s.' % str(fn))
+    if not asyncio.iscoroutinefunction(fn) and not inspect.isgeneratorfunction(fn):
+        fn = asyncio.coroutine(fn)
+    logging.info('add route %s %s => %s(%s)' % (method, path, fn.__name__, ', '\
+                .join(inspect.signature(fn).parameters.keys())))
+    app.router.add_route(method, path, RequestHandler(app, fn))
+
+
+## 定义add_routes函数，自动把handler模块的所有符合条件的URL函数注册了
+def add_routes(app, module_name):
+    n = module_name.rfind('.')
+    if n == (-1):
+        mod = __import__(module_name, globals(), locals())
+    else:
+        name = module_name[n+1:]
+        mod = getattr(__import__(module_name[:n], globals(), locals(), [name]), name)
+    for attr in dir(mod):
+        if attr.startswith('_'):
+            continue
+        fn = getattr(mod, attr)
+        if callable(fn):
+            method = getattr(fn, '__method__', None)
+            path = getattr(fn, '__route__', None)
+            if method and path:
+                add_route(app, fn)
 
